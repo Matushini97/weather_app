2cedc09254159fcbfa4ace7c46477686b8d37bc2
Ilya Matsiushyn <matushin.97@gmail.com>
2023-06-19 18:09:00 +0300

    feat: finished upcomingWeather add City screen

--- src/screens/UpcomingWeather.js
+++ src/screens/UpcomingWeather.js
@@ -1,6 +1,7 @@
 import React from "react";
-import {Text, SafeAreaView, StyleSheet, FlatList, View, StatusBar} from "react-native";
+import {Text, SafeAreaView, StyleSheet, FlatList, View, StatusBar, Image, ImageBackground} from "react-native";
 import {Feather} from "@expo/vector-icons";
+import ListItem from "../components/ListItem";
 
 const DATA = [
     {
@@ -42,26 +43,12 @@ const DATA = [
 
 ]
 
-const Item = (props) => {
-    const {
-        dt_text,
-        min,
-        max,
-        condition
-    } = props
-    return (
-        <View style={styles.item}>
-            <Feather name='sun' size={50} color={'white'} />
-            <Text style={styles.date}>{dt_text}</Text>
-            <Text style={styles.temp}>{min}</Text>
-            <Text style={styles.temp}>{max}</Text>
-        </View>
-    )
-}
+
 const UpcomingWeather = () => {
+    const {container, image} = styles
     const renderItem = ({item}) => {
         return (
-            <Item
+            <ListItem
                 condition={item.weather[0].main}
                 dt_text={item.dt_text}
                 min={item.main.temp_min}
@@ -70,14 +57,18 @@ const UpcomingWeather = () => {
             )
     }
     return (
-        <SafeAreaView style={styles.container}>
-            <Text>Upcoming Weather</Text>
-            <FlatList
-                data={DATA}
-                renderItem={renderItem}
-                keyExtractor={(item) => item.dt_text}
-            />
-
+        <SafeAreaView style={container}>
+            <ImageBackground
+                source={require('../../assets/upcoming-back.jpg')}
+                style={image}
+            >
+                <Text>Upcoming Weather</Text>
+                <FlatList
+                    data={DATA}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.dt_text}
+                />
+             </ImageBackground>
         </SafeAreaView>
     );
 };
@@ -86,25 +77,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         marginTop: StatusBar.currentHeight || 0,
-        backgroundColor: 'red'
+        backgroundColor: 'royalblue'
     },
-    item: {
-        padding: 20,
-        marginVertical: 8,
-        marginHorizontal: 16,
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        borderWidth: 5,
-        backgroundColor: 'pink'
-    },
-    temp: {
-        color: 'white',
-        fontSize: 20,
-    },
-    date: {
-        color: 'white',
-        fontSize: 15,
+    image: {
+        flex: 1
     }
 })
 
